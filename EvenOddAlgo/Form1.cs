330f55b7a6f6e90088bf5a9a73fa1b9cdc7bac89
Julian <Julian-Tekstra@hotmail.com>
2020-05-18 13:51:15 +0200

    Even/Odd Algorithm

---
+++ EvenOddAlgo/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvenOddAlgo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsEven(int input)
+        {
+            int result = input % 2;
+            if (result == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           label1.Text = IsEven((int)numericUpDown1.Value).ToString();
+           label1.Visible = true;
+        }
+    }
+}
